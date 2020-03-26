008dccaa28116a541f23ec1ecd9594588daba599
Bmercado17 <askotala@yahoo.com>
2020-03-25 18:35:10 -0700

    third commit

--- script/script.js
+++ script/script.js
@@ -1,1 +1,18 @@
-$(document).ready(function() {});
+$(document).ready(function() {
+  function startOverlay(overlayId) {
+    $(".overlay-veil").fadeTo("300", "0.6", function() {
+      var element = $('div[data-overlayItem="' + overlayId + '"]');
+      element.fadeIn("3000");
+      element.addClass("active");
+    });
+  }
+
+  function closeOverlayAll(overlayId) {
+    startOverlay(overlayId);
+  }
+  $(".overlay-item-click").on("click", function(e) {
+    e.preventDefault();
+    var overlayId = $(this).data("overlay");
+    closeOverlayAll(overlayId);
+  });
+});
